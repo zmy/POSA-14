9f827be2bdc3fc92dbff73ae1d1ee6b9dcef372b
zmy <mengyu.chou@gmail.com>
2014-05-30 22:07:37 -0700

    Assignment 0

--- assignments/week-1-assignment-0/src/edu/vuum/mooca/SynchronizedQueue.java
+++ assignments/week-1-assignment-0/src/edu/vuum/mooca/SynchronizedQueue.java
@@ -220,21 +220,27 @@ public class SynchronizedQueue {
             // initialization below to create two Java Threads, one
             // that's passed the producerRunnable and the other that's
             // passed the consumerRunnable.
-            Thread consumer = null;
-            Thread producer = null;
+            Thread consumer = new Thread(consumerRunnable);
+            Thread producer = new Thread(producerRunnable);
 
             // TODO - you fill in here to start the threads. More
             // interesting results will occur if you start the
             // consumer first.
+            consumer.start();
+            producer.start();
             
             // Give the Threads a chance to run before interrupting
             // them.
             Thread.sleep(100);
 
             // TODO - you fill in here to interrupt the threads.
+            consumer.interrupt();
+            producer.interrupt();
 
             // TODO - you fill in here to wait for the threads to
             // exit.
+            consumer.join();
+            producer.join();
             
             // Do some sanity checking to see if the Threads work as
             // expected.
